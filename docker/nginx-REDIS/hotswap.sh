bb0026fc565321c1ded83e43736869387904d4c4
Michael Field <michaelcfield97@gmail.com>
2019-03-13 23:54:40 -0700

    + better than {

--- docker/nginx-REDIS/hotswap.sh
+++ docker/nginx-REDIS/hotswap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 CONF_FILE="/etc/nginx/nginx.conf"
-sed -i 's/redis:\|[0-9.]\{12\}:/'"$1"':/g' "$CONF_FILE"
+sed -i 's/redis:\|[0-9.]\+:/'"$1"':/g' "$CONF_FILE"
 /usr/sbin/nginx -s reload
