420765fee79db770c4a0fdea059b1c6d5a2970b7
Michael Field <michaelcfield97@gmail.com>
2019-03-13 23:39:15 -0700

    DONT WORK

--- docker/nginx-REDIS/hotswap.sh
+++ docker/nginx-REDIS/hotswap.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 CONF_FILE="/etc/nginx/nginx.conf"
-if grep -q $1 "$CONF_FILE"; then
-  sed -i 's/redis:\|[0-9.]\{12\}:/'"$1"':/g' "$CONF_FILE"
-  /usr/sbin/nginx -s reload
-fi
+sed -i 's/redis:\|[0-9.]\{12\}:/'"$1"':/g' "$CONF_FILE"
+/usr/sbin/nginx -s reload
